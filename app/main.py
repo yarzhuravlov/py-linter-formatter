057eb20e7ce25f6b2cca4c6af4bddec6d0acfa6f
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-01-30 18:52:31 +0200

    fix: change if len(errors) check to just if errors

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
             format_linter_error(error) for error in errors
         ],
         "path": file_path,
-        "status": "passed" if len(errors) == 0 else "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
